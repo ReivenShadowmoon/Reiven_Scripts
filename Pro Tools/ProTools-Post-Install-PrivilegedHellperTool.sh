5805be56846d51956824740b1b21ef8398b7de1c
ReivenShadowmoon <88510347+ReivenShadowmoon@users.noreply.github.com>
2021-09-07 13:29:55 -0700

    Update ProTools-Post-Install-PrivilegedHellperTool.sh
    Added Comments for each comment in the Shell Script

--- Pro Tools/ProTools-Post-Install-PrivilegedHellperTool.sh
+++ Pro Tools/ProTools-Post-Install-PrivilegedHellperTool.sh
@@ -6,9 +6,16 @@
 #
 
 # Copy the com.avid.bsd.shoe Helper Tool
+#Set variable appName to the PHT (Privileged Helper Tool) for Avid Pro Tools
 appName=$(ls -t -U /Applications/Pro\ Tools.app/Contents/Library/LaunchServices/ | grep -m 1 "com.avid" )
+
+#Path and file name using $appName for the PHT
 PHT_SHOE=/Library/PrivilegedHelperTools/$appName
 
+#Copies PHT from with in the Pro Tools.app file into /Library/PHT
 /bin/cp -f /Applications/Pro\ Tools.app/Contents/Library/LaunchServices/$appName $PHT_SHOE
+
+#Sets the corect owner for the PHT
 /usr/sbin/chown root:wheel $PHT_SHOE
+#Sets the needed permissions so that PHT will work correctly
 /bin/chmod 544 $PHT_SHOE
