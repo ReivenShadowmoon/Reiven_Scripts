1d9b8b8a0887219b10d022d12a42ebec1cd211a5
ReivenShadowmoon <88510347+ReivenShadowmoon@users.noreply.github.com>
2021-09-29 11:02:47 -0700

    Script clean up.
    Added more comments and corrected a typo in the Fusion results line.

--- JAMF_Extension_Attributes/Apple_Computer_Drive_type.sh
+++ JAMF_Extension_Attributes/Apple_Computer_Drive_type.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-#Checked is storage device is SSD or HDD
+#Checked is storage device is Fusion Drive or SSD
 SSD=$(/usr/sbin/diskutil info / | /usr/bin/awk '/Solid State/{print $NF}')
 FUS=$(/usr/sbin/diskutil info / | /usr/bin/awk '/Fusion Drive/{print $NF}')
 
-#Takes results from the check and displays SSD or HDD
+#Takes results from the check and displays SSD if Fusion Drive is No
 if [ $SSD == Yes ] && [ $FUS == No ]; then
 
 /bin/echo "<result>SSD</result>"
 
+#If SSD is Yes and Fusion Drive is Yes then resilts wil be Fusion
 elif [ $FUS == Yes ]; then
 
-/bin/echo "<result>Fusion</resuly>"
+/bin/echo "<result>Fusion</result>"
 
+#If both SSD and Fusion Drive are No then device is HDD
 else
 
 /bin/echo "<result>HDD</result>"
